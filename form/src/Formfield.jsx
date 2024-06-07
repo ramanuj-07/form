8cf14cfffca7f0c6bc2e7901772af3baedf15d80
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-06-07 10:29:16 +0530

    moved validate from formfield to Validate

--- form/src/Formfield.jsx
+++ form/src/Formfield.jsx
@@ -24,37 +24,6 @@ function Formfield() {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
-    const validate = () => {
-        let formErrors = {};
-
-        if (!formData.firstName) formErrors.firstName = 'First Name is required';
-        if (!formData.lastName) formErrors.lastName = 'Last Name is required';
-        if (!formData.username) formErrors.username = 'Username is required';
-        if (!formData.email) formErrors.email = 'Email is required';
-
-        if (!formData.password)
-            formErrors.password = 'Password is required';
-        else if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(formData.password)) {
-            formErrors.password = 'Password must contain at least 8 characters, including letters and numbers';
-        }
-
-        if (!formData.phoneNo) formErrors.phoneNo = 'Phone Number is required';
-        if (!formData.country) formErrors.country = 'Country is required';
-        if (!formData.city) formErrors.city = 'City is required';
-        if (!formData.panNo)
-            formErrors.panNo = 'PAN Number is required';
-        else if (!/^[A-Z]{5}\d{4}[A-Z]$/.test(formData.panNo)) {
-            formErrors.panNo = 'Incorrect PAN Number';
-        }
-        if (!formData.aadharNo) formErrors.aadharNo = 'Aadhar Number is required';
-        else if (!/^\d{12}$/.test(formData.aadharNo)) {
-            formErrors.aadharNo = 'Incorrect Aadhar Number';
-        }
-
-        setErrors(formErrors);
-
-        return Object.keys(formErrors).length === 0;
-    };
     // const handleSubmit = (e) => {
     //     e.preventDefault();
     //     if (validate()) {
--- form/src/Validate.jsx
+++ form/src/Validate.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
 function Validate() {
-  return (
-    <div>Validate</div>
-  )
+
+    let formErrors = {};
+
+    if (!formData.firstName) formErrors.firstName = 'First Name is required';
+    if (!formData.lastName) formErrors.lastName = 'Last Name is required';
+    if (!formData.username) formErrors.username = 'Username is required';
+    if (!formData.email) formErrors.email = 'Email is required';
+
+    if (!formData.password)
+        formErrors.password = 'Password is required';
+    else if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(formData.password)) {
+        formErrors.password = 'Password must contain at least 8 characters, including letters and numbers';
+    }
+
+    if (!formData.phoneNo) formErrors.phoneNo = 'Phone Number is required';
+    if (!formData.country) formErrors.country = 'Country is required';
+    if (!formData.city) formErrors.city = 'City is required';
+    if (!formData.panNo)
+        formErrors.panNo = 'PAN Number is required';
+    else if (!/^[A-Z]{5}\d{4}[A-Z]$/.test(formData.panNo)) {
+        formErrors.panNo = 'Incorrect PAN Number';
+    }
+    if (!formData.aadharNo) formErrors.aadharNo = 'Aadhar Number is required';
+    else if (!/^\d{12}$/.test(formData.aadharNo)) {
+        formErrors.aadharNo = 'Incorrect Aadhar Number';
+    }
+
+    setErrors(formErrors);
+
+    return Object.keys(formErrors).length === 0;  
 }
 
 export default Validate;
