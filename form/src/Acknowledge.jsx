f785bfcd50bee2c144072a2a6dc3cf7493fc3de1
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-06-07 21:04:52 +0530

    Update Acknowledge.jsx

--- form/src/Acknowledge.jsx
+++ form/src/Acknowledge.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 
 function Acknowledge() {
     const location = useLocation();
   const { formData } = location.state || {};
 
   return (
-      <div>
-          <h1>Form Submitted Successfully!</h1>
+      <Container>
+          <Title>Form Submitted Successfully!</Title>
       {formData && (
-        <div>
+        <FormData>
           <p><strong>First Name:</strong> {formData.firstName}</p>
           <p><strong>Last Name:</strong> {formData.lastName}</p>
           <p><strong>Username:</strong> {formData.username}</p>
@@ -19,11 +20,35 @@ function Acknowledge() {
           <p><strong>City:</strong> {formData.city}</p>
           <p><strong>PAN No:</strong> {formData.panNo}</p>
           <p><strong>Aadhar No:</strong> {formData.aadharNo}</p>
-        </div>
+        </FormData>
       )}
-      </div>
+      </Container>
       
   )
 }
 
 export default Acknowledge;
+
+const Container = styled.div`
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #f9f9f9;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+const Title = styled.h1`
+    font-size: 24px;
+    margin-bottom: 20px;
+`;
+
+const FormData = styled.div`
+    p {
+        margin-bottom: 10px;
+    }
+
+    strong {
+        margin-right: 5px;
+    }
+`;
--- form/src/Validate.jsx
+++ form/src/Validate.jsx
@@ -6,13 +6,7 @@ const Validate=(formData,setErrors) => {
     if (!formData.lastName) formErrors.lastName = 'Last Name is required';
     if (!formData.username) formErrors.username = 'Username is required';
     if (!formData.email) formErrors.email = 'Email is required';
-
-    if (!formData.password)
-        formErrors.password = 'Password is required';
-    // else if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(formData.password)) {
-    //     formErrors.password = 'Password must contain at least 8 characters, including letters and numbers';
-    // }
-
+    if (!formData.password)formErrors.password = 'Password is required';
     if (!formData.phoneNo) formErrors.phoneNo = 'Phone Number is required';
     if (!formData.country) formErrors.country = 'Country is required';
     if (!formData.city) formErrors.city = 'City is required';
@@ -27,7 +21,7 @@ const Validate=(formData,setErrors) => {
     }
 
     setErrors(formErrors);
-    
+
     return formErrors;
 }
 
